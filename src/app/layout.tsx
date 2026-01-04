78e4499674733ac483d19e5040c14d990d2d9679
César Rodríguez Lins <ironsand@gmail.com>
2026-01-04 15:05:48 +0100

    feature: world map background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "./providers";
 import "./globals.css";
+import classNames from "classnames";
 import NavBar from "../shared/components/macro/navigation";
 
 const geistSans = Geist({
@@ -20,9 +21,17 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const { x } = await getBackgroundPositionByIp();
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background dark:bg-foreground`}>
+      <body
+        className={classNames(
+          geistSans.variable,
+          geistMono.variable,
+          "antialiased bg-background bg-[url(@/../public/sketch-world-map.png)] bg-cover dark:bg-foreground"
+        )}
+        style={{ backgroundPositionX: x }}
+      >
         <Providers>
           <NavBar />
           {children}
@@ -30,4 +39,28 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
       </body>
     </html>
   );
+}
+
+async function getBackgroundPositionByIp() {
+  const FALLBACK_LAT = 40;
+  const FALLBACK_LONG = 0;
+  try {
+    const locationResp = await fetch("https://geolocation-db.com/json/");
+    const { latitude, longitude } = await locationResp.json();
+    return convertLatLongToPercent({ lat: latitude, long: longitude });
+  } catch (e) {
+    console.log("Unable to fetch location", e);
+    return convertLatLongToPercent({ lat: FALLBACK_LAT, long: FALLBACK_LONG });
+  }
+}
+
+function convertLatLongToPercent({ lat, long }: { lat: number; long: number }) {
+  const HEMISPHERE_LONGITUDE_DEGREES = 180;
+  const HEMISPHERE_LATITUDE_DEGREES = 90;
+  const decimalY = (long / HEMISPHERE_LONGITUDE_DEGREES) * 100;
+  const y = `${Math.floor(decimalY)}%`;
+  const decimalX = (lat / HEMISPHERE_LATITUDE_DEGREES) * 100;
+  const x = `${Math.floor(decimalX)}%`;
+
+  return { x, y };
 }
