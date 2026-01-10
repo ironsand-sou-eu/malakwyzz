6ac2b57442179de8424eaf244a1f0ab12cc19b3e
César Rodríguez Lins <ironsand@gmail.com>
2026-01-10 21:55:16 +0100

    internal: biome format

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -8,16 +8,13 @@ export default async function Countries() {
   const t = await getTranslations();
 
   return (
-    <main
-      className="m-auto flex w-full max-w-3xl py-10 px-16 flex-col items-center justify-between bg-background dark:bg-foreground sm:items-start"
-      style={{ minHeight: "calc(100dvh - 40px)" }}
-    >
+    <>
       <div>
         <Image className="mx-auto" src={malakwizzLogo} alt="Malakwizz logo" width={300} height={300} priority />
         <Title>{t("geo-geeks-gather")}</Title>
         <Subtitle>{t("try-your-luck")}</Subtitle>
       </div>
       <GameSelectorBlock />
-    </main>
+    </>
   );
 }
